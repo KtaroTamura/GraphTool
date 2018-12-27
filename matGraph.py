57d2001e8a43f91d43dbaeaac0314f06d8dbfe9e
KtaroTamura <tamu02wa.ma@gmail.com>
2018-12-27 17:49:58 +0900

    numpy->pandas

--- matGraph.py
+++ matGraph.py
@@ -1,19 +1,26 @@
 import sys
-import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__=="__main__":
+##Get Data set 
 	args=sys.argv
 	filename=args[1]
 	data=[]
-	data=np.loadtxt(filename,delimiter=',',dtype=None,skiprows=0)
-	x=data[:,0]
-	y=data[:,1]
+	data=pd.read_csv(filename,delimiter=',',dtype=None,skiprows=0)
+
+##Make graph data
+	data.colums["","",""]
+	x=data["1"]
+	y=data["2"]
 	plt.plot(x,y,label='',color='b')
-	plt.title('')
-	plt.xlabel(u'')
-	plt.ylabel(u'')
+
+##plot details
+#	plt.title('AAA')
+#	plt.xlabel(u'aaa')
+#	plt.ylabel(u'bbb')
 #	plt.xlim([])
 #	plt.ylim([])
+#	plt.yscale('log')
 #	plt.legend(loc='upper right')
-	plt.show()
+	plt.show(block=False)
