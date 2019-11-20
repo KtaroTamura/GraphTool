12394d0545c157f3a9ce66fdba082db219f5c22d
KtaroTamura <tamu02wa.ma@gmail.com>
2019-11-20 20:35:53 +0900

    Add Legend

--- graph.cxx
+++ graph.cxx
@@ -11,11 +11,18 @@ int graph(){
 	}
 
 ///Graph Setting///
-//  g1->SetTitle("Title");
+//	g1->SetTitle("Title");
 //	g1->GetXaxis()->SetTitle("XX");
 //	g1->GetYaxis()->SetTitle("YY");
 
+	//Legend //
+//	TLegend *leg=new TLegend(0.65,0.65,0.85,0.85);
+//  leg->AddEntry(g1,"g1 name","l");
+//  leg->AddEntry(g2,"g2 name","l");
+
+
 	g1->Draw();
 	fclose(fp);
+
 	return 0;
 }
